cd46a342693925cbfd92afe9dc4c1402c2c99beb
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-28 19:20:21 +0530

    Code Tx and Rx and data logger

---
+++ XBEE_ARDUNIO_TX/XBEE_ARDUNIO_TX.ino
@@ -0,0 +1,30 @@
+ 
+  
+//#include <Wire.h> //BH1750 IIC Mode 
+//#include <math.h> 
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+void setup()
+{
+ // Wire.begin();
+  lcd.begin(20,4);
+  Serial.begin (9600);
+}
+  
+  
+  
+void loop()
+{ 
+   int sensorValue = analogRead(A0);
+   int FSR1=sensorValue/2;
+   lcd.print(FSR1);
+   Serial.println(FSR1);
+   delay(2000);
+    lcd.clear();
+}
+  
+  
+  
+  
